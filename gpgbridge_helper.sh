e1e3b7b3568a859070e33380bf28b36ad84b2ca5
Magnus Hyllander <magnus@hyllander.org>
2020-09-15 12:04:28 +0200

    Updated helper script

--- gpgbridge_helper.sh
+++ gpgbridge_helper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 #--------------------------------------------------------------------------
 # GPG bridging from WSL gpg to gpg4win gpg-agent.exe
 # (needed to use a Yubikey, since WSL cannot access USB devices)
@@ -6,24 +6,25 @@
 set -eu
 
 usage() {
-   echo "Usage: gpgbridge_helper [ --ssh ] [ --wsl2 ] [ -q --quiet ] start|stop|restart"
+    echo "Usage: gpgbridge_helper [ --ssh ] [ --wsl2 ] [ -q | --quiet ] start|stop|restart"
 }
 
 # PATHS
-SCRIPT_DIR=/mnt/c/Program1/
+SCRIPT_DIR=/mnt/c/Program1/gpgbridge
 
-PIDFILE_WSL=~/.gpgbridge_wsl.pid
-LOGFILE_WSL=~/.gpgbridge_wsl.log
+PIDFILE_WSL="$HOME/.gpgbridge_wsl.pid"
+LOGFILE_WSL="$HOME/.gpgbridge_wsl.log"
 
-_pidfile_win=$SCRIPT_DIR/gpgbridge_win.pid
-_logfile_win=$SCRIPT_DIR/gpgbridge_win.log
-touch $_pidfile_win $_logfile_win  # Needs to be created otherwise gpgbridge complains
+_pidfile_win="$SCRIPT_DIR/gpgbridge_win.pid"
+_logfile_win="$SCRIPT_DIR/gpgbridge_win.log"
+
+touch "$_pidfile_win" "$_logfile_win"  # Needs to be created otherwise wslpath complains
 PIDFILE_WIN=$(wslpath -wa $_pidfile_win)
 LOGFILE_WIN=$(wslpath -wa $_logfile_win)
 
 SCRIPT_FILE_NAME=gpgbridge.rb
 
-SCRIPT_PATH_WSL=$SCRIPT_DIR/$SCRIPT_FILE_NAME
+SCRIPT_PATH_WSL="$SCRIPT_DIR/$SCRIPT_FILE_NAME"
 
 start_gpgbridge()
 {
@@ -35,19 +36,19 @@ start_gpgbridge()
     args="$(_quote "$SCRIPT_PATH_WSL") --daemon"
 
     if [ -n "${PIDFILE_WSL:-}" ] ; then
-       args="$args --pidfile $(_quote "$PIDFILE_WSL")"
+	args="$args --pidfile $(_quote "$PIDFILE_WSL")"
     fi
 
     if [ -n "${LOGFILE_WSL:-}" ] ; then
-       args="$args --logfile $(_quote "$LOGFILE_WSL")"
+	args="$args --logfile $(_quote "$LOGFILE_WSL")"
     fi
 
     if [ -n "${PIDFILE_WIN:-}" ] ; then
-       args="$args --windows-pidfile $(_quote "$PIDFILE_WIN")"
+	args="$args --windows-pidfile $(_quote "$PIDFILE_WIN")"
     fi
 
     if [ -n "${LOGFILE_WIN:-}" ] ; then
-       args="$args --windows-logfile $(_quote "$LOGFILE_WIN")"
+	args="$args --windows-logfile $(_quote "$LOGFILE_WIN")"
     fi
 
     if [ "$SSH" -eq 1 ] ; then
@@ -57,14 +58,12 @@ start_gpgbridge()
     fi
 
     if [ "$WSL2" -eq 1 ] ; then
-       wsl_ip="$(ip route | awk '/^default via / {print $3}')"
-       args="$args --remote-address $wsl_ip"
+	wsl_ip="$(ip route | awk '/^default via / {print $3}')"
+	args="$args --remote-address $wsl_ip"
     fi
 
     if [ "$QUIET" -eq 1 ] ; then
         args="$args >/dev/null 2>&1"
-    else
-        args="$args --verbose"
     fi
 
     eval ruby "$args"
@@ -72,8 +71,8 @@ start_gpgbridge()
 
 stop_gpgbridge()
 {
-   # Kill gpgbridge if running, else return
-   pkill -TERM -f 'ruby.*gpgbridge\.rb' || return 0
+    # Kill gpgbridge if running, else return
+    pkill -TERM -f 'ruby.*gpgbridge\.rb' || return 0
 }
 
 restart_gpgbridge()
@@ -96,30 +95,30 @@ PARSED_ARGS=$(getopt -a -n gpgbridge_helper -o qh --long quiet,ssh,wsl2,help -- 
 IS_ARGS_VALID=$?
 
 if [ ! $IS_ARGS_VALID ] ; then
-   usage
-   exit 1
+    usage
+    exit 1
 fi
 
 eval set -- "$PARSED_ARGS"
 while :
 do
-   case "$1" in
-      --ssh)            SSH=1   ; shift ;;
-      --wsl2)           WSL2=1  ; shift ;;
-      -q | --quiet)     QUIET=1 ; shift ;;
-      -h | --help)      usage && exit 0 ;;
-      --) shift ; break ;;
-   esac
+    case "$1" in
+	--ssh)            SSH=1   ; shift ;;
+	--wsl2)           WSL2=1  ; shift ;;
+	-q | --quiet)     QUIET=1 ; shift ;;
+	-h | --help)      usage && exit 0 ;;
+	--) shift ; break ;;
+    esac
 done
 
 if [ -z "${1:-}" ] ; then
-   usage
-   exit 1
+    usage
+    exit 1
 fi
 
 case "$1" in
-   start)   start_gpgbridge ;;
-   stop)    stop_gpgbridge ;;
-   restart) restart_gpgbridge ;;
-   *)       usage && exit 1 ;;
+    start)   start_gpgbridge ;;
+    stop)    stop_gpgbridge ;;
+    restart) restart_gpgbridge ;;
+    *)       usage && exit 1 ;;
 esac
